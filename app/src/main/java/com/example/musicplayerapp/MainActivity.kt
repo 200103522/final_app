1d4eb52688b2b575a9f68d0dab17bc492a26df9a
Zhassur Tashpulat <200103522@stu.sdu.edu.kz>
2022-12-06 11:11:01 +0600

    app_binding

--- app/src/main/java/com/example/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.musicplayerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicplayerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.shuffleBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
