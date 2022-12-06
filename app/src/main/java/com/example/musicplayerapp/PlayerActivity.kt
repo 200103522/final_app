9c30941445f0e44b357934649795bff12d116c54
Zhassur Tashpulat <200103522@stu.sdu.edu.kz>
2022-12-06 22:07:24 +0600

    app_remove_system_UI

--- app/src/main/java/com/example/musicplayerapp/PlayerActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/PlayerActivity.kt
@@ -1,11 +1,16 @@
 package com.example.musicplayerapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.ACCESSIBILITY_LIVE_REGION_NONE
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_player)
     }
 }
